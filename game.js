4837a44b7f5019aefe6235eb8f4e68856dfdae17
denizen91c0Tebu <niklauslevi372@gmail.com>
2024-03-08 13:04:57 -0500

    allow user to play round against computer

--- game.js
+++ game.js
@@ -14,3 +14,44 @@ function getComputerChoice() {
         return "scissors"
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = prompt("Please type Rock, Paper, or Scissors");
+    console.log(playerChoice)
+    if (playerChoice == null) {
+        return "You have canceled the program"
+    }
+    playerSelection = playerChoice.toLowerCase();
+    if (playerSelection != "rock" && playerSelection != "scissors" && playerSelection != "paper") {
+        return "Sorry you must input one of those 3 options!"
+    }
+    computerSelection = getComputerChoice();
+    console.log(computerSelection)
+    if (playerSelection == computerSelection) {
+        return "It's a tie!"
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore ++;
+        return "You Lose! Paper beats rock"
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerScore ++;
+        return "You Win! Congratulations"
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore ++;
+        return "You Win! Congratulations"
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore ++;
+        return "You Lose! Scissors beats paper"
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore ++;
+        return "You Win! Congratulations"
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore ++;
+        return "You Lose! Rock beats scissors"
+    }
+}
